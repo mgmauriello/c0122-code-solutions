c100cdab34d5b6c20c5f7b4668897a29ff8d1446
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-01 15:23:49 -0800

    created new arrays for both orders and items.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,32 +1,71 @@
 var orderHistory = [
   {
-    item: ['JavaScript for impatient programmers'],
-    author: ['Rauschmayer, Dr. Axel'],
-    price: ['$31.55'],
-    orderPlaced: ['August 4, 2020'],
-    orderDelivered: ['Aug 8, 2020']
+    orderNumber: '11439416898772232',
+    shipTo: 'JS Master',
+    total: '#34.00',
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'Aug 8, 2020',
+    item: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        price: '$31.55',
+        returnBy: 'Sep 7, 2020'
+      }
+    ]
   },
   {
-    item: ['The Timeless Way of Building'],
-    author: ['Alexander, Christopher'],
-    price: ['$41.33'],
-    orderPlaced: ['July 19, 2020'],
-    orderDelivered: ['Jul 20, 2020']
+    orderNumber: '11399842681280257',
+    shipTo: 'JS Master',
+    total: '$44.53',
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'Jul 20, 2020',
+    item: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        price: '$41.33',
+        returnBy: 'Aug 19, 2020'
+      }
+    ]
   },
   {
-    item: ['Gamecube Controller Adapter Super Smash Bros Switch Gamecube Adapter for WI U, PC Support Turbo and Vibration Features No Driver and No Lag Gamecube Adapter'],
-    price: ['$15.98'],
-    orderPlaced: ['July 4, 2020'],
-    orderDelivered: ['Jul 7, 2020']
+    orderNumber: '11428755579059409',
+    shipTo: 'JS Master',
+    total: '$17.22',
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'Jul 7, 2020',
+    item: [
+      {
+        type: 'electornic',
+        product: 'Gamecube Controller Adapter Super Smash Bros Switch Gamecube Adapter for WI U, PC Support Turbo and Vibration Features No Driver and No Lag Gamecube Adapter',
+        price: '$15.98',
+        returnBy: 'Aug 5, 2020'
+      }
+    ]
   },
   {
-    item: ['GameCube Controller Super Smash Bros.Edition(Nintendo Switch)', 'The Art of Sql'],
-    author: ['', 'Faroult, Stephane'],
-    price: ['$94.95', '$33.99'],
-    orderPlaced: ['July 3, 2020'],
-    orderDelivered: ['Jul 5, 2020']
+    orderNumber: 11328831772648248,
+    shipTo: 'JS Master',
+    total: '$17.22',
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'Jul 5, 2020',
+    item: [
+      {
+        type: 'electronic',
+        product: 'GameCube Controller Super Smash Bros.Edition(Nintendo Switch)',
+        price: '$94.95',
+        returnBy: 'Aug 4, 2020'
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        price: '$33.99',
+        returnBy: 'Aug 4, 2020'
+      }
+    ]
   }
 ];
-
-console.log('On ' + orderHistory[2].orderPlaced + ' I ordered a ' + orderHistory[2].item + ' for ' + orderHistory[2].price + '.');
-console.log('On ' + orderHistory[3].orderPlaced + ' I ordered ' + orderHistory[3].item[1] + ' for ' + orderHistory[3].price[1] + '.');
