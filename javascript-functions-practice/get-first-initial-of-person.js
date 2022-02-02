f010ce91798bd1383c483b6e53fc9a560dab746f
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-01 17:37:21 -0800

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-first-initial-of-person.js
@@ -0,0 +1,5 @@
+/* exported getFirstInitialOfPerson */
+function getFirstInitialOfPerson(person) {
+  person = person.firstName[0];
+  return person;
+}
---
+++ javascript-functions-practice/get-length-of-array.js
@@ -0,0 +1,5 @@
+/* exported getLengthOfArray */
+function getLengthOfArray(array) {
+  array = array.length;
+  return array;
+}
