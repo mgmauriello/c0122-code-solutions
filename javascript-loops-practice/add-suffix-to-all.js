a44c7ef20124d1d472fc5fe7bc012e1b6101dd76
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-07 10:53:12 -0800

    pushed newArr.

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -2,7 +2,7 @@
 function addSuffixToAll(words, suffix) {
   var newArr = [];
   for (var i = 0; i < words.length; i++) {
-    newArr[i] = words[i] + suffix;
+    newArr.push(words[i] + suffix);
   }
   return newArr;
 }
