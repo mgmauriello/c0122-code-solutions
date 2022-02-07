feddcb078568485fc90ef179ab2be8d2c6a6f094
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-07 11:07:35 -0800

    pause work

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,17 @@
 /* exported ransomCase */
+/*
+-need to make every other char uppercase
+-need to put chars into a new string
+-function firstLetterUppercase (input) {
+  for (i=0; i < input.length; i+=2) {
+     input = input.substr(0, i) + input[i].toUpperCase() + input.substr(i + 1);
+  }
+  return input;
+}
+*/
+function ransomCase(string) {
+  for (var i = 0; i < string.length; i += 2) {
+    string = string.substring(0, i) + string.toUppercase() + string.substring(i + 1);
+  }
+  return string;
+}
