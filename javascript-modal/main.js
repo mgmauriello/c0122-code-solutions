4b90a53b2994cc452cbc98706b150b731f45f6fa
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-11 15:39:48 -0800

    pause

---
+++ javascript-modal/main.js
@@ -0,0 +1,15 @@
+/*
+var $modal = document.querySelector('.modal');
+var $trigger = document.querySelector('.trigger');
+var $closeButton = document.querySelector('.close-button');
+
+$trigger.addEventListener('click', function (event) {
+  if (!event.target.matches('.trigger')) {
+    return;
+  }
+  $modal.className = 'modal show-modal'
+
+}
+$closeButton.addEventListener('click', function (event)') {
+
+*/
