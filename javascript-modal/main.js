0f2ab18e75b38aa556d0a3da1e9f498a19164e92
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-11 16:45:27 -0800

    Completed javascript-modal challenge.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,15 +1,19 @@
-/*
 var $modal = document.querySelector('.modal');
 var $trigger = document.querySelector('.trigger');
-var $closeButton = document.querySelector('.close-button');
 
-$trigger.addEventListener('click', function (event) {
+function handleTriggerClick(event) {
   if (!event.target.matches('.trigger')) {
     return;
   }
-  $modal.className = 'modal show-modal'
-
+  $modal.className = 'modal show-modal';
 }
-$closeButton.addEventListener('click', function (event)') {
 
-*/
+function handleModalClick(event) {
+  if (!event.target.matches('.modal-button')) {
+    return;
+  }
+  $modal.remove('show-modal');
+}
+
+$trigger.addEventListener('click', handleTriggerClick);
+$modal.addEventListener('click', handleModalClick);
