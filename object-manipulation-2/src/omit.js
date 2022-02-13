dc6a62f2f99f1c266e821ff1c6e7ed6b6b1b0127
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-13 13:27:32 -0800

    Completed object-manipulation-2.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,10 +1,19 @@
-/* exported omit */
+/* exported omit
+-need to get a new obj lit to hold chosen objs
+-If key is listed in keys, should not be included newObj.
+-newObj of all of the properties of source not listed in keys
+-opposite of pick where we do NOT want to get properties
+-loop through keys and search through the source
+for each of those keys
+-for with an if statement
+*/
+
 function omit(source, keys) {
-  var result = {};
-  for (var key in source) {
-    if (keys.includes(key)) {
-      result[key] = source[key];
+  var newObj = {};
+  for (const key in source) {
+    if (!keys.includes(key)) {
+      newObj[key] = source[key];
     }
   }
-  return result;
+  return newObj;
 }
