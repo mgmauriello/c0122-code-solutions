919b05592c5518d73758aef5cbe6c49b3e986db4
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-17 09:31:59 -0800

    pause work

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,20 +3,30 @@ create whole string to lowercase
 separate each word in string to own string
 uppercase each word, capitalize first char by using slice
 join back together
-hard code in, the, of,
-*/
-function titleCase(title) {
-  title = title.toLowerCase();
-  title = title.split(' ');
+hard code javascript and api
+regex
+  title = title.toLowerCase().split(' ');
 
   for (var i = 0; i < title.length; i++) {
+    var regex = '\b(?<!^)(a(?:nd?)?|the|to|[io]n|from|with)(?!$)\b'
+    if (title.match())
     title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
+
+function titleCase(title) {
+  title = title.toLowerCase().split(' ');
+
+  for (var i = 0; i < title.length; i++) {
+    var regex = '\b(?<!^)(a(?:nd?)?|the|to|[io]n|from|with)(?!$)\b';
+    if (title.match()) { title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1); }
+
+    if (title === 'javascript') {
+      return 'JavaScript';
+    }
+    if (title === 'api') {
+      return 'API';
+    }
+    return title.join(' ');
+
   }
-  if (title === 'javascript') {
-    return 'JavaScript';
-  }
-  if (title === 'api') {
-    return 'API';
-  }
-  return title.join(' ');
 }
+*/
