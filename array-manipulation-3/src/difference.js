01d3b485ab0f13d096c5eb16ac74eecb181daf0f
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-02-18 09:31:56 -0800

    pause work on array-manipulation-3.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,4 +1,15 @@
 /* exported difference */
 function difference(first, second) {
-
+  var newArr = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) === -1) {
+      newArr.push(first[i]);
+    }
+  }
+  for (var a = 0; a < second.length; a++) {
+    if (first.indexOf(second[a]) === -1) {
+      newArr.push(second[a]);
+    }
+  }
+  return newArr;
 }
