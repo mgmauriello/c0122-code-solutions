ccabb59e9fded87552da45515716659133d8f7d9
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-16 19:58:03 +0000

    Completed node-require exercise.

---
+++ node-require/divide.js
@@ -0,0 +1,4 @@
+module.exports = function divideSum(a, b) {
+  const quotient = parseInt(a) / parseInt(b);
+  return quotient;
+};
---
+++ node-require/subtract.js
@@ -0,0 +1,4 @@
+module.exports = function subtractSum(a, b) {
+  const difference = parseInt(a) - parseInt(b);
+  return difference;
+};
