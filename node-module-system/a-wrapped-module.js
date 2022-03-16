745a4d3cb55c79cad9db92e18e1baf66003d221c
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-16 17:47:05 +0000

    Completed node-module-system exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('exports typeof:', typeof exports);
+console.log(exports);
+
+console.log('require typeof:', typeof require);
+console.log(require);
+
+console.log('module typeof:', typeof module);
+console.log(module);
+
+console.log(' __filename typeof:', typeof __filename);
+console.log(__filename);
+
+console.log('__dirname typeof:', typeof __dirname);
+console.log(__dirname);
