e44dadf43858edc30ae97d222518c332de33a5e0
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-16 18:46:10 +0000

    added value labels to console logs

--- node-module-system/a-wrapped-module.js
+++ node-module-system/a-wrapped-module.js
@@ -1,14 +1,14 @@
 console.log('exports typeof:', typeof exports);
-console.log(exports);
+console.log('value of exports', exports);
 
 console.log('require typeof:', typeof require);
-console.log(require);
+console.log('value of require', require);
 
 console.log('module typeof:', typeof module);
-console.log(module);
+console.log('value of module', module);
 
 console.log(' __filename typeof:', typeof __filename);
-console.log(__filename);
+console.log('value of __filename', __filename);
 
 console.log('__dirname typeof:', typeof __dirname);
-console.log(__dirname);
+console.log('value of __dirname', __dirname);
