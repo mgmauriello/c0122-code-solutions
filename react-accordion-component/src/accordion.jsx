9478392814c129e16d9d970f331f706c78b8865b
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-04-01 06:04:07 +0000

    added topics as prop in index.jsx.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,28 +1,5 @@
 import React from 'react';
 
-const topics = [
-  {
-    id: '001',
-    tab: 'Hisoka Morow',
-    content: ` Hisoka is a Transmuter endowed with a powerful, sinister aura
-      that disquieted Wing and Gon even after the latter learned Nen.`
-
-  },
-  {
-    id: '002',
-    tab: 'Killua Zoldyck',
-    content: ` As a Transmuter, Killua's most efficient in changing the
-      properties and shape of his aura. Just like Gon's, Killua's talent
-      is said to be absolutely astounding, found only in one out of ten million people.`
-  },
-  {
-    id: '003',
-    tab: 'Chrollo Lucilfer',
-    content: ` Chrollo is a Specialist. His talent for Nen is judged prodigious
-      not only for his age but even by the standards of masters in the discipline.`
-  }
-];
-
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
@@ -46,6 +23,7 @@ class Accordion extends React.Component {
   }
 
   render() {
+    const topics = this.props.topics;
     const listedTopics = topics.map(topic =>
       <li key={topic.id}>
         <h3 onClick={this.handleClick} id={topic.id} className="tab">
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const topics = [
+  {
+    id: '001',
+    tab: 'Hisoka Morow',
+    content: ` Hisoka is a Transmuter endowed with a powerful, sinister aura
+      that disquieted Wing and Gon even after the latter learned Nen.`
+
+  },
+  {
+    id: '002',
+    tab: 'Killua Zoldyck',
+    content: ` As a Transmuter, Killua's most efficient in changing the
+      properties and shape of his aura. Just like Gon's, Killua's talent
+      is said to be absolutely astounding, found only in one out of ten million people.`
+  },
+  {
+    id: '003',
+    tab: 'Chrollo Lucilfer',
+    content: ` Chrollo is a Specialist. His talent for Nen is judged prodigious
+      not only for his age but even by the standards of masters in the discipline.`
+  }
+];
+
 ReactDOM.render(
-  <Accordion />,
+  <Accordion topics={topics}/>,
   document.querySelector('#root')
 );
