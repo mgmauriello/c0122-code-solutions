c0f6d13d0de1d0c33e40de6e3f4bee38ab9bdb3f
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-05-13 23:20:14 +0000

    Commented out solution to add react hook solution.

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class HotButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clickCount: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+// export default class HotButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       clickCount: 0
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick() {
-    this.setState({ clickCount: this.state.clickCount + 1 });
-  }
+//   handleClick() {
+//     this.setState({ clickCount: this.state.clickCount + 1 });
+//   }
 
-  render() {
-    if (this.state.clickCount <= 3) {
-      return <button className='cold' onClick={this.handleClick}>Hot Button!</button>;
-    } else if (this.state.clickCount <= 6) {
-      return <button className='cool' onClick={this.handleClick}>Hot Button!</button>;
-    } else if (this.state.clickCount <= 9) {
-      return <button className='tepid' onClick={this.handleClick}>Hot Button!</button>;
-    } else if (this.state.clickCount <= 12) {
-      return <button className='warm' onClick={this.handleClick}>Hot Button!</button>;
-    } else if (this.state.clickCount <= 15) {
-      return <button className='hot' onClick={this.handleClick}>Hot Button!</button>;
-    } else if (this.state.clickCount <= 18 || this.state.clickCount > 18) {
-      return <button className='nuclear' onClick={this.handleClick}>Hot Button!</button>;
-    } else {
-      return <button className='cold' onClick={this.handleClick}>Hot Button!</button>;
-    }
+//   render() {
+//     if (this.state.clickCount <= 3) {
+//       return <button className='cold' onClick={this.handleClick}>Hot Button!</button>;
+//     } else if (this.state.clickCount <= 6) {
+//       return <button className='cool' onClick={this.handleClick}>Hot Button!</button>;
+//     } else if (this.state.clickCount <= 9) {
+//       return <button className='tepid' onClick={this.handleClick}>Hot Button!</button>;
+//     } else if (this.state.clickCount <= 12) {
+//       return <button className='warm' onClick={this.handleClick}>Hot Button!</button>;
+//     } else if (this.state.clickCount <= 15) {
+//       return <button className='hot' onClick={this.handleClick}>Hot Button!</button>;
+//     } else if (this.state.clickCount <= 18 || this.state.clickCount > 18) {
+//       return <button className='nuclear' onClick={this.handleClick}>Hot Button!</button>;
+//     } else {
+//       return <button className='cold' onClick={this.handleClick}>Hot Button!</button>;
+//     }
+//   }
+// }
+
+export default function HotButton() {
+
+  const [count, setCount] = useState(0);
+
+  if (count <= 3) {
+    return <button className='cold' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else if (count <= 6) {
+    return <button className='cool' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else if (count <= 9) {
+    return <button className='tepid' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else if (count <= 12) {
+    return <button className='warm' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else if (count <= 15) {
+    return <button className='hot' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else if (count <= 18 || count > 18) {
+    return <button className='nuclear' onClick={() => setCount(count + 1)}>Hot Button!</button>;
+  } else {
+    return <button className='cold' onClick={() => setCount(count + 1)}>Hot Button!</button>;
   }
 }
