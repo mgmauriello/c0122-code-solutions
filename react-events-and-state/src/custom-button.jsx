344012f1e45f297a97a3f478f25510bf7deb9f9e
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-05-13 23:53:23 +0000

    Commented out solution to add react hook solution for custom button.

--- react-events-and-state/src/custom-button.jsx
+++ react-events-and-state/src/custom-button.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class CustomButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isClicked: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
+// export default class CustomButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isClicked: false };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick() {
-    this.setState({ isClicked: true });
-  }
+//   handleClick() {
+//     this.setState({ isClicked: true });
+//   }
 
-  render() {
-    if (this.state.isClicked === false) {
-      return (
-        <button onClick={this.handleClick}>Click Me!!</button>
-      );
-    } else {
-      return (
-        <button>Thanks!</button>
-      );
-    }
+//   render() {
+//     if (this.state.isClicked === false) {
+//       return (
+//         <button onClick={this.handleClick}>Click Me!!</button>
+//       );
+//     } else {
+//       return (
+//         <button>Thanks!</button>
+//       );
+//     }
+//   }
+// }
+
+export default function CustomButton() {
+  const [clicked, setClick] = useState(false);
+
+  if (clicked) {
+    return <button onClick={() => setClick()}>Click Me!!</button>;
+  } else {
+    return <button>Thanks!</button>;
   }
 }
