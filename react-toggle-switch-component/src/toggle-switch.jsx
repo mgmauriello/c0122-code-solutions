ee17cf0c21343f56b9396e10f7e26436c8942403
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-05-14 00:34:19 +0000

    Updated toggle switch to useState react hook

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class ToggleSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOn: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function ToggleSwitch() {
+  const [isClicked, setClick] = useState(true);
 
-  handleClick() {
-    if (this.state.isOn === true) {
-      this.setState({ isOn: false });
-    } else {
-      this.setState({ isOn: true });
-    }
-  }
+  const handleClick = () => {
+    setClick(isClicked => !isClicked);
+  };
 
-  render() {
-    if (this.state.isOn === true) {
-      return (
-        <button>
-          <div onClick={this.handleClick} className='toggle-on'></div>
-          <div onClick={this.handleClick} className='circle-on'></div>
-          <div className='text'>On</div>
-        </button>
-      );
-    } else {
-      return (
-        <button>
-          <div onClick={this.handleClick} className='toggle-off'></div>
-          <div onClick={this.handleClick} className='circle-off'></div>
-          <div className='text'>Off</div>
-        </button>
-      );
-    }
-  }
+  return (
+    <button>
+      <div onClick={handleClick} className={isClicked ? 'toggle-off' : 'toggle-on'}></div>
+      <div onClick={handleClick} className={isClicked ? 'circle-off' : 'circle-on'}></div>
+      <div className='text'>{isClicked ? 'OFF' : 'ON'}</div>
+    </button>
+
+  );
 }
+
+// export default class ToggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isOn: false };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     if (this.state.isOn === true) {
+//       this.setState({ isOn: false });
+//     } else {
+//       this.setState({ isOn: true });
+//     }
+//   }
+
+//   render() {
+//     if (this.state.isOn === true) {
+//       return (
+//         <button>
+//           <div onClick={this.handleClick} className='toggle-on'></div>
+//           <div onClick={this.handleClick} className='circle-on'></div>
+//           <div className='text'>On</div>
+//         </button>
+//       );
+//     } else {
+//       return (
+//         <button>
+//           <div onClick={this.handleClick} className='toggle-off'></div>
+//           <div onClick={this.handleClick} className='circle-off'></div>
+//           <div className='text'>Off</div>
+//         </button>
+//       );
+//     }
+//   }
+// }
