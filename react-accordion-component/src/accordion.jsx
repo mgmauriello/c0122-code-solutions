d435cced7daf6826395c709ddb8d19a191c56abd
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-05-16 17:32:24 -0700

    refactored accodrion react component for react hook use

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,45 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class Accordion extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentView: null
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function Accordion(props) {
+  const [currentView, setView] = useState(null);
 
-  handleClick(event) {
+  const handleClick = event => {
     const clickedID = event.target.id;
-    if (this.state.currentView === clickedID) {
-      this.setState({
-        currentView: null
-      });
+    if (currentView === clickedID) {
+      setView(null)
     } else {
-      this.setState({
-        currentView: clickedID
-      });
+      setView(clickedID)
     }
   }
 
-  render() {
-    const topics = this.props.topics;
-    const listedTopics = topics.map(topic =>
-      <li key={topic.id}>
-        <h3 onClick={this.handleClick} id={topic.id} className="tab">
-          {topic.tab}
-        </h3>
-        <p className={this.state.currentView === topic.id ? 'content' : 'content hidden'}>
-          {topic.content}
-        </p>
-      </li>
-    );
-    return (
+  const topics = props.topics;
+
+  const listedTopics = topics.map(topic =>
+    <li key={topic.id}>
+      <h3 onClick={handleClick} id={topic.id} className="title">
+        {topic.title}
+      </h3>
+      <p className={currentView === topic.id
+        ? 'content'
+        : 'content hidden'
+      }>
+        {topic.content}
+      </p>
+    </li>
+  );
+
+  return (
       <ul>
         {listedTopics}
       </ul>
     );
-  }
 }
 
-export default Accordion;
+// class Accordion extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentView: null
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick(event) {
+//     const clickedID = event.target.id;
+//     if (this.state.currentView === clickedID) {
+//       this.setState({
+//         currentView: null
+//       });
+//     } else {
+//       this.setState({
+//         currentView: clickedID
+//       });
+//     }
+//   }
+
+//   render() {
+//     const topics = this.props.topics;
+//     const listedTopics = topics.map(topic =>
+//       <li key={topic.id}>
+//         <h3 onClick={this.handleClick} id={topic.id} className="tab">
+//           {topic.tab}
+//         </h3>
+//         <p className={this.state.currentView === topic.id ? 'content' : 'content hidden'}>
+//           {topic.content}
+//         </p>
+//       </li>
+//     );
+//     return (
+//       <ul>
+//         {listedTopics}
+//       </ul>
+//     );
+//   }
+// }
