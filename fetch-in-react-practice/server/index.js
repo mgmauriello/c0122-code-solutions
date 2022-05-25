4ad4f063c2ed5e31913af39cbf2e7fa9c03cd8de
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-05-25 20:04:46 +0000

    aysnc await query in fetch req practice

--- fetch-in-react-practice/server/index.js
+++ fetch-in-react-practice/server/index.js
@@ -38,28 +38,9 @@ app.get('/api/todos', async (req, res) => {
       error: 'an unexpected error occurred'
     });
   }
-
 });
 
-// app.get('/api/todos', (req, res) => {
-//   const sql = `
-//     select *
-//       from "todos"
-//      order by "todoId"
-//   `;
-//   db.query(sql)
-//     .then(result => {
-//       res.json(result.rows);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({
-//         error: 'an unexpected error occurred'
-//       });
-//     });
-// });
-
-app.post('/api/todos', (req, res) => {
+app.post('/api/todos', async (req, res) => {
   const { task, isCompleted = false } = req.body;
   if (!task || typeof isCompleted !== 'boolean') {
     res.status(400).json({
@@ -73,20 +54,21 @@ app.post('/api/todos', (req, res) => {
     returning *
   `;
   const params = [task, isCompleted];
-  db.query(sql, params)
-    .then(result => {
-      const [todo] = result.rows;
-      res.status(201).json(todo);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({
-        error: 'an unexpected error occurred'
-      });
+
+  try {
+    const query = db.query(sql, params);
+    const result = await query;
+    const [todo] = result.rows;
+    res.status(201).json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
     });
+  }
 });
 
-app.patch('/api/todos/:todoId', (req, res) => {
+app.patch('/api/todos/:todoId', async (req, res) => {
   const todoId = Number(req.params.todoId);
   if (!Number.isInteger(todoId) || todoId < 1) {
     res.status(400).json({
@@ -109,24 +91,112 @@ app.patch('/api/todos/:todoId', (req, res) => {
      returning *
   `;
   const params = [isCompleted, todoId];
-  db.query(sql, params)
-    .then(result => {
-      const [todo] = result.rows;
-      if (!todo) {
-        res.status(404).json({
-          error: `cannot find todo with todoId ${todoId}`
-        });
-        return;
-      }
-      res.json(todo);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({
-        error: 'an unexpected error occurred'
+
+  try {
+    const query = db.query(sql, params);
+    const result = await query;
+    const [todo] = result.rows;
+    if (!todo) {
+      res.status(404).json({
+        error: `cannot find todo with todoId ${todoId}`
       });
+      return;
+    }
+    res.json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
     });
+  }
 });
+
+// app.get('/api/todos', (req, res) => {
+//   const sql = `
+//     select *
+//       from "todos"
+//      order by "todoId"
+//   `;
+//   db.query(sql)
+//     .then(result => {
+//       res.json(result.rows);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({
+//         error: 'an unexpected error occurred'
+//       });
+//     });
+// });
+
+// app.post('/api/todos', (req, res) => {
+//   const { task, isCompleted = false } = req.body;
+//   if (!task || typeof isCompleted !== 'boolean') {
+//     res.status(400).json({
+//       error: 'task (string) and isCompleted (boolean) are required fields'
+//     });
+//     return;
+//   }
+//   const sql = `
+//     insert into "todos" ("task", "isCompleted")
+//     values ($1, $2)
+//     returning *
+//   `;
+//   const params = [task, isCompleted];
+//   db.query(sql, params)
+//     .then(result => {
+//       const [todo] = result.rows;
+//       res.status(201).json(todo);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({
+//         error: 'an unexpected error occurred'
+//       });
+//     });
+// });
+
+// app.patch('/api/todos/:todoId', (req, res) => {
+//   const todoId = Number(req.params.todoId);
+//   if (!Number.isInteger(todoId) || todoId < 1) {
+//     res.status(400).json({
+//       error: 'todoId must be a positive integer'
+//     });
+//     return;
+//   }
+//   const { isCompleted } = req.body;
+//   if (typeof isCompleted !== 'boolean') {
+//     res.status(400).json({
+//       error: 'isCompleted (boolean) is a required field'
+//     });
+//     return;
+//   }
+//   const sql = `
+//     update "todos"
+//        set "updatedAt"   = now(),
+//            "isCompleted" = $1
+//      where "todoId" = $2
+//      returning *
+//   `;
+//   const params = [isCompleted, todoId];
+//   db.query(sql, params)
+//     .then(result => {
+//       const [todo] = result.rows;
+//       if (!todo) {
+//         res.status(404).json({
+//           error: `cannot find todo with todoId ${todoId}`
+//         });
+//         return;
+//       }
+//       res.json(todo);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({
+//         error: 'an unexpected error occurred'
+//       });
+//     });
+// });
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
